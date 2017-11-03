029fba2d66df451ca20bd3fe62940418d9885125
Andrew Donshik <andrew@donshikfamily.com>
2017-11-03 09:40:38 -0400

    board population
    broke printboard

--- chess.py
+++ chess.py
@@ -1,50 +1,36 @@
 import string
-'''class Piece(object):
-    def __init__(self, pos, color):
+class Piece(object):
+    def __init__(self, color):
         self.passthrough = False
-        self.position = pos
-        self.captured = False
         self.color = color
     def setpos(location):
         self.position = location
 
 class Pawn(Piece):
-    def move(location):
-    
-    def capture(location):
-    
-    def promote(location):
-
+    def __init__(self, color):
+        Piece.__init__(self,color)
+        self.candouble = True
 class Knight(Piece):
-    def __init__(self, pos, color):
-        Piece.init(self, pos, color)
+    def __init__(self, color):
+        Piece.__init__(self, color)
         self.passthrough = True
-    def move(location):
-
 class Bishop(Piece):
-    def move(location):
+    None
 
 class Rook(Piece):
-    def __init__(self, pos, color):
-        Piece.init(self, pos, color)
+    def __init__(self, color):
+        Piece.__init__(self, color)
         self.cancastle = True
-    def move(location):
-    
-    def castle(location):
 
 class Queen(Piece):
-    def move(location):
+    None
 
 class King(Piece):
-    def __init__(self, pos, color):
-        Piece.__init__(self, pos, color)
+    def __init__(self, color):
+        Piece.__init__(self, color)
         self.incheck = False
         self.cancastle = True
-    def move(location):
 
-    def castle(location):
-    
-'''
 class Board(object):
     def __init__(self, dim1,dim2):
         self.dim = (dim1,dim2)
@@ -65,3 +51,18 @@ class Board(object):
         for i in range(1, len(self.array[0]) + 1):
             rangelist += ' ' + str(i)
         print('X ' + rangelist)
+
+class Game(object):
+    def __init__(self):
+        self.piecedict= {' ': ' ', 'K' : King('White'), 'N' : Knight('White'), 'P': Pawn('White'), 'Q': Queen('White'), 'R' : Rook('White'), 'B': Bishop('White'),'k' : King('Black'), 'n' : Knight('Black'), 'p': Pawn('Black'), 'q': Queen('Black'), 'r' : Rook('Black'), 'b': Bishop('Black')}
+        self.initpos = [[]]
+    def populate(self):
+        self.board.array = self.initpos
+        for i in range(len(self.board.array)):
+            for j in range(len(self.board.array[i])):
+                self.board.array[i][j] = self.piecedict[self.board.array[i][j]]
+class MicroChess(Game):
+    def __init__(self):
+        Game.__init__(self)
+        self.board = Board(5,4)
+        self.initpos = [['k', 'b', 'n', 'r'], ['p', ' ', ' ', ' '], [' ', ' ', ' ', ' '],[' ', ' ', ' ', 'P'], ['R', 'N', 'B', 'K']]
