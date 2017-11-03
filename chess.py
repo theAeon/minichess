ee2834ed9a966c72156e93ef44c76f747c5f806c
Andrew Donshik <andrew@donshikfamily.com>
2017-11-03 08:55:10 -0400

    working printboard

--- chess.py
+++ chess.py
@@ -49,11 +49,19 @@ class Board(object):
     def __init__(self, dim1,dim2):
         self.dim = (dim1,dim2)
         self.array = []
+        self.createarray()
     def createarray(self):
-        for row in range(self.dim[0]):
+        for column in range(self.dim[0]):
             self.array.append([])
-            for column in range(self.dim[1]):
-                self.array[row].append(None)
+            for row in range(self.dim[1]):
+                self.array[column].append(' ')
     def printboard(self):
-        for row in board:
-            print(self.array)
+        for column in range(len(self.array)):
+            columnstr = ''
+            for i in range(len(self.array[column])):
+                columnstr += ' '  + self.array[column][i]
+            print(str(self.dim[0] - column) + ' ' + columnstr)
+        rangelist = ''
+        for i in range(1, len(self.array[0]) + 1):
+            rangelist += ' ' + str(i)
+        print('X ' + rangelist)
