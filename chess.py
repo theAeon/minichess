4fb9a9322e3db6d7077a593f59c01a6b776ce638
Andrew Donshik <andrew@donshikfamily.com>
2017-11-03 15:20:28 -0400

    final commit for day
    reachable needs fixing

--- chess.py
+++ chess.py
@@ -1,13 +1,12 @@
-import string
 class Piece(object):
     def __init__(self, color):
         self.passthrough = False
         self.color = color
 class Pawn(Piece):
     def __init__(self, color):
-        Piece.__init__(self,color)
+        Piece.__init__(self, color)
         self.candouble = True
-        movecount = 2
+        self.movecount = 2
         self.validmoves = ((1, 0))
         self.validcaptures = ((1, 1))
 class Knight(Piece):
@@ -21,20 +20,20 @@ class Bishop(Piece):
     def __init__(self, color):
         Piece.__init__(self, color)
         self.movecount = '-1'
-        self.validmoves = ((1, 1), (-1, 1), (1,-1), (-1,-1))
+        self.validmoves = ((1, 1), (-1, 1), (1, -1), (-1, -1))
         self.validcaptures = self.validmoves
 class Rook(Piece):
     def __init__(self, color):
         Piece.__init__(self, color)
         self.cancastle = True
         self.movecount = '-1'
-        self.validmoves = ((1,0), (0,1), (-1, 0), (0,-1))
+        self.validmoves = ((1, 0), (0, 1), (-1, 0), (0, -1))
         self.validcaptures = self.validmoves
 class Queen(Piece):
     def __init__(self, color):
         Piece.__init__(self, color)
         self.movecount = '-1'
-        self.validmoves = ((1,0), (0,1), (1,1), (-1, 0), (0, -1), (-1, 1), (1, -1), (-1, -1))
+        self.validmoves = ((1, 0), (0, 1), (1, 1), (-1, 0), (0, -1), (-1, 1), (1, -1), (-1, -1))
         self.validcaptures = self.validmoves
 
 class King(Piece):
@@ -43,35 +42,36 @@ class King(Piece):
         self.incheck = False
         self.cancastle = True
         self.movecount = '1'
-        self.validmoves = ((1,0), (0,1), (1,1), (0, -1), (-1, 0), (-1, -1), (-1, 1), (-1, 0))
+        self.validmoves = ((1, 0), (0, 1), (1, 1), (0, -1), (-1, 0), (-1, -1), (-1, 1), (-1, 0))
         self.validcaptures = self.validmoves
 
 class Board(object):
-    def __init__(self, dim1,dim2):
-        self.dim = (dim1,dim2)
+    def __init__(self, dim1, dim2):
+        self.dim = (dim1, dim2)
         self.array = []
         self.readablearray = []
         self.createarray()
     def createarray(self):
         for column in range(self.dim[0]):
             self.array.append([])
-            for row in range(self.dim[1]):
+            for i in range(self.dim[1]):
                 self.array[column].append(' ')
     def printboard(self):
         for column in range(len(self.readablearray)):
             columnstr = ''
             for i in range(len(self.readablearray[column])):
                 columnstr += ' '  + self.readablearray[column][i]
-            print(str(self.dim[0] - column) + ' ' + columnstr)
+            print(str(self.dim[0] - column - 1) + ' ' + columnstr)
         rangelist = ''
-        for i in range(1, len(self.readablearray[0]) + 1):
+        for i in range(0, len(self.readablearray[0])):
             rangelist += ' ' + str(i)
         print('X ' + rangelist)
 
 class Game(object):
     def __init__(self):
-        self.piecedict= {' ': ' ', 'K' : King('White'), 'N' : Knight('White'), 'P': Pawn('White'), 'Q': Queen('White'), 'R' : Rook('White'), 'B': Bishop('White'),'k' : King('Black'), 'n' : Knight('Black'), 'p': Pawn('Black'), 'q': Queen('Black'), 'r' : Rook('Black'), 'b': Bishop('Black')}
+        self.piecedict= {' ' : ' ', 'K' : King('White'), 'N' : Knight('White'), 'P': Pawn('White'), 'Q': Queen('White'), 'R' : Rook('White'), 'B': Bishop('White'), 'k' : King('Black'), 'n' : Knight('Black'), 'p': Pawn('Black'), 'q': Queen('Black'), 'r' : Rook('Black'), 'b': Bishop('Black')}
         self.initpos = ([])
+        self.board = Board(1, 1)
     def populate(self):
         self.board.readablearray = self.initpos
         for i in range(len(self.board.array)):
@@ -82,17 +82,19 @@ class Game(object):
         loc = self.board.array[end[0]][end[1]]
         if piece == ' ':
             return False
-        if loc != ' '
+        if loc != ' ':
             if piece.color != loc.color:
-                if self.reachable(piece, end):
+                if self.reachable(piece, start, end, piece.validmoves, piece.validcaptures, piece.movecount):
                     self.board.array[end[0]][end[1]] = piece
-                    self.board.readablearray[end[0]][end[1]] = self.readablearray[start[0]][start[1]]
-        if piece == ' ':
-            if self.reachable(piece, end):
+                    self.board.readablearray[end[0]][end[1]] = self.board.readablearray[start[0]][start[1]]
+        if loc == ' ':
+            if self.reachable(piece, start, end, piece.validmoves, piece.validcaptures, piece.movecount):
                 self.board.array[end[0]][end[1]] = piece
-                self.board.readablearray[end[0]][end[1]] = self.readablearray[start[0]][start[1]]
+                self.board.readablearray[end[0]][end[1]] = self.board.readablearray[start[0]][start[1]]
+        else:
+            return False
 
-    def reachable(piece, location, target, validmoves, validcaptures, movecount):
+    def reachable(self, piece, location, target, validmoves, validcaptures, movecount):
         try:
             if location == target:
                 return True
@@ -102,9 +104,9 @@ class Game(object):
                 for i in validmoves:
                     newloc = (location[0] + i[0], location[1] + i[1])
                     if self.board.readablearray[newloc[0], newloc[1]] == ' ' and piece.passthrough == False:
-                        return reachable(piece, newloc, target, i, i, movecount - 1)
+                        return self.reachable(piece, newloc, target, i, i, movecount - 1)
                     if piece.passthrough == True:
-                        return reachable(piece, newloc, target, i, i, movecount - 1)
+                        return self.reachable(piece, newloc, target, i, i, movecount - 1)
             if validmoves != validcaptures:
                 for i in validcaptures:
                     if self.board.readablearray[location[0] + i[0]][location[1] + i[1]] == True:
@@ -112,7 +114,7 @@ class Game(object):
             
                 for i in validmoves:
                     newloc = (location[0] + i[0], location[1] + i[1])
-                    return reachable(piece, newloc, target, i, 'nope', movecount - 1)
+                    return self.reachable(piece, newloc, target, i, 'nope', movecount - 1)
         except IndexError:
             return False
 class MicroChess(Game):
